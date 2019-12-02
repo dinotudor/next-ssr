1c5fdd1ce25a20bbe42c0b3d9b33f0ceaa3d0213
Dino Tudor <dinotudor@gmail.com>
2019-12-02 23:14:39 +0100

    add next-routes dep, routes.js, fix handler in server.js, add dev script

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const next = require('next');
 const routes = require('./routes');
 
 const app = next({ dev: process.env.NODE_ENV !== 'production ' });
+const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
   createServer(handler).listen(3000);
